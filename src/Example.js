4a3e8680d0bcc7c9bd9a7bf72c5ac53f72b52d62
Ronald Suez <rsuez@mediastre.am>
2018-08-07 15:30:26 -0400

    adding example

--- src/Example.js
+++ src/Example.js
@@ -1,60 +1,51 @@
-//  Created by react-native-create-bridge
-
-import React, { Component } from 'react'
-import { NativeModules, StyleSheet } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, NativeModules } from 'react-native';
 import RNtimerEventEmitter from './RNTimerEventEmitter'
-
-const RNtimer = NativeModules.RNTimer
-
-export default class RNTimerView extends Component {
-
-  state = {
-    time: 0,
-    started: false,
-    stopped: true
-  }
-
-
-  getStatus = () => {
-    const states = ['started', 'stopped']
-    const started = this.state.started && !this.state.stopped
-    const stopped = !this.state.started && this.state.stopped
-    const currentStatus = [started, stopped].findIndex((i) => i)
-    return states[currentStatus]
-  }
-
-  componentDidMount() {
-
-    RNtimerEventEmitter.addListener('onTimeStarted', (res) => {
-      this.setState({started: true, stopped: false}, () => {
-        if(typeof this.props.onTimeStarted === 'function') {
-          this.props.onTimeStarted(res)
-        }
-      })
-    })
-
-    RNtimerEventEmitter.addListener('onTimeFinished', (res) => {
-      this.setState({started: false, stopped: true}, () => {
-        if(typeof this.props.onTimeFinished === 'function') {
-          this.props.onTimeFinished(res)
-        }
-      })
-    })
-  }
-
-  componentWillUnmount() {
-    RNtimerEventEmitter.removeAllListeners()
-  }
-
-  delayedTimer(interval, callback = () => null, options = {singleCall: true}) {
-    RNtimer.delayedTimer(interval, callback)
-  }
-
+import RNtimerView from './RNTimerView'
+export default class Example extends React.Component {
+  
+ state = {
+   time: 60
+ }
   render () {
-    return this.props.children({
-      delayedTimer: this.delayedTimer,
-      status: this.getStatus()
-    })
+    return (
+      <RNtimerView>
+      {({delayedTimer, status}) => (
+        <View style={styles.container}>
+          {this.state.started && <Text>Timer set for {this.state.time} seconds.</Text>}
+          <View style={styles.row}>
+            <View style={styles.separator} />
+            <TouchableOpacity style={styles.button} onPress={() => delayedTimer(this.state.time)}>
+              <Text style={styles.text}>Set Interval</Text>
+            </TouchableOpacity>
+          </View>
+          <Text>Timer {status}</Text>
+        </View>
+      )}
+    </RNtimerView>
+    )
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  text: {
+    color: 'white'
+  },
+  button: {
+    padding: 20,
+    width: 120,
+    backgroundColor: 'blue'
+  },
+  separator: {
+    width: 10
+  }
+});
